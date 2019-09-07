7f877d600614942cddf744d993d2b64a6b94f5f3
zzyybs <zzyybs@126.com>
2019-09-07 21:27:23 +0800

    del ...

--- src/main/java/com/atguigu/github/Hello_GitHub.java
+++ src/main/java/com/atguigu/github/Hello_GitHub.java
@@ -9,10 +9,5 @@ public class Hello_GitHub
     public static void main(String[] args)
     {
         System.out.println("11111");
-        System.out.println("mysql03");
-        System.out.println("java01");
-        System.out.println("abcabc");
-        System.out.println("mycomputer");
-        System.out.println("github");
     }
 }
