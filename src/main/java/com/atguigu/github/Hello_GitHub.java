5851b7167398281a3f5911d8854a173f0f7ff8c2
zzyybs <zzyybs@126.com>
2019-09-07 20:56:47 +0800

    new project

---
+++ src/main/java/com/atguigu/github/Hello_GitHub.java
@@ -0,0 +1,13 @@
+package com.atguigu.github;
+
+/**
+ * @auther zzyy
+ * @create 2019-09-07 20:53
+ */
+public class Hello_GitHub
+{
+    public static void main(String[] args)
+    {
+        System.out.println("11111");
+    }
+}
