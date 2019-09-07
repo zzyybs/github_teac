9e7132636b0d25b0d58c9627af68d5dccbe2dc3b
zzyybs <zzyybs@126.com>
2019-09-07 21:17:19 +0800

    update Java01
    update Java01

--- src/main/java/com/atguigu/github/Hello_GitHub.java
+++ src/main/java/com/atguigu/github/Hello_GitHub.java
@@ -9,6 +9,6 @@ public class Hello_GitHub
     public static void main(String[] args)
     {
         System.out.println("11111");
-        System.out.println("22222");
+        System.out.println("java01");
     }
 }
